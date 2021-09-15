73f7503afd9f12ad0e1fc1b1b437dd91a7000f6a
ch <mkmsacc@outlook.com>
2021-09-15 12:07:21 +0700

    Add MovieController

---
+++ src/main/java/com/example/demo/movie/MovieController.java
@@ -0,0 +1,36 @@
+package com.example.demo.movie;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/movies")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping
+    public List<Movie> listMovies() {
+        return movieService.getMovies();
+    }
+
+    @GetMapping("{id}")
+    public Movie getMovieId(@PathVariable("id") Integer id) {
+        return movieService.getMovie(id);
+    }
+
+    @PostMapping
+    public void addMovie(@RequestBody Movie movie) {
+        movieService.addNewMovie(movie);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteMovie(@PathVariable("id") Integer id) {
+        movieService.deleteMovie(id);
+    }
+
+    // TODO: Update movie
+}

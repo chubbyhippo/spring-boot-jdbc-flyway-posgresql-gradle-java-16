4cca2844148b81cf47b76913a5a406a01bc30c09
ch <mkmsacc@outlook.com>
2021-09-29 22:14:46 +0700

    Configure jdbc template

--- src/main/java/com/example/demo/movie/MovieDataAccessService.java
+++ src/main/java/com/example/demo/movie/MovieDataAccessService.java
@@ -1,12 +1,18 @@
 package com.example.demo.movie;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class MovieDataAccessService implements MovieDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
     public List<Movie> selectMovies() {
         throw new UnsupportedOperationException("not implemented");
